9b62e827cc906f10041b0eeabb26c03c1bc7fabf
KopoJJang <2201110369@office.kopo.ac.kr>
2023-04-11 15:07:53 +0900

    ex3 ~ exLink

--- src/main/java/com/example/ex3/controller/SampleController.java
+++ src/main/java/com/example/ex3/controller/SampleController.java
@@ -1,11 +1,12 @@
-package kr.ac.poly.controller;
+package com.example.ex3.controller;
 
-import kr.ac.poly.dto.SampleDTO;
+import com.example.ex3.dto.SampleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,12 +17,12 @@ import java.util.stream.IntStream;
 @RequestMapping("/sample")
 @Log4j2
 public class SampleController {
-    @GetMapping("/test1")
+    @GetMapping("/ex1")
     public void ex1(){
-        log.info("test1의 정보..");
+        log.info("ex1의 정보..");
     }
 
-    @GetMapping({"/ex2"})
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model){
         List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(
                 i -> {
@@ -36,4 +37,23 @@ public class SampleController {
 
         model.addAttribute("list", list);
     }
+
+    @GetMapping("exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("Fis...100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "Success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3() 호출 됨");
+    }
 }
+
