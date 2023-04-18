7a496c2703047dacd1edeb2ddf5ce4e57404675d
KopoJJang <2201110369@office.kopo.ac.kr>
2023-04-18 15:14:20 +0900

    0418_exTemplate_fragment

--- src/main/java/com/example/ex3/controller/SampleController.java
+++ src/main/java/com/example/ex3/controller/SampleController.java
@@ -56,7 +56,7 @@ public class SampleController {
         log.info("ex3() 호출 됨");
     }
 
-    @GetMapping("/exLayout")
+    @GetMapping({"/exLayout", "/exTemplate", "/exSidebar"})
     public void exLayout1(){
         log.info("exLayout() 호출됨");
     }
@@ -70,5 +70,6 @@ public class SampleController {
     public void exLayout3(){
         log.info("exLayout3() 호출됨");
     }
+
 }
 
